8fc116995115103428e2753dd9abbf6c587da662
diegomoyano87 <diegomoyano_19@hotmail.com>
2022-10-11 00:42:59 -0300

    Se modifico el formato de la Tienda y la tarjeta del producto

--- V2/src/app/components/menu/menu.component.ts
+++ V2/src/app/components/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() {
   }
